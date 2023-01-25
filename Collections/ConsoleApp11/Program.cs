0ccfa228d32409d02afe953dc733c53c2543d329
İslam Salamzadə <islam.salamzade.191@gmail.com>
2023-01-25 14:51:30 +0400

    Collections

---
+++ Collections/ConsoleApp11/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+Dictionary<int, string> dict = new();
+
+dict.Add(1, "islam");
+
+dict.Add(2, "namiq");
+
+Console.WriteLine("Dictionary");
+
+foreach (var item in dict)
+{
+    Console.WriteLine(item);
+}
+
+SortedList<int,string> list = new();
+
+list.Add(2, "namiq");
+list.Add(1, "islam");
+
+Console.WriteLine("Sorted List");
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+
+Hashtable table = new();
+
+table.Add(1, "islam");
+table.Add(2, "namiq");
+
+Console.WriteLine("Hashtable");
+
+foreach (DictionaryEntry item in table)
+{
+    Console.WriteLine($"{item.Key} {item.Value}");
+
+}
+
